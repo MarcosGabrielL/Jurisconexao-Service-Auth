2c20489f86f8c5e1ff062383e4f7379a64a76564
Marcos Gabriel Lopes <marcos.lopes@cooperflora.com.br>
2023-04-28 15:47:03 -0300

    models initial

---
+++ src/main/java/br/com/jurisconexao/auth/Auth/AuthApplication.java
@@ -0,0 +1,14 @@
+package br.com.jurisconexao.auth.Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
+  
---
+++ src/main/java/br/com/jurisconexao/auth/Auth/models/EventoIntercafe.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.jurisconexao.auth.Auth.models;
+
+/**
+ *
+ * @author Marcos
+ */
+public interface EventoIntercafe {
+    
+     public Evento saveEvento(Evento person);
+}
