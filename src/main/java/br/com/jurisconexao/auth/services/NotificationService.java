619ce35582db313afa02d849de20c4a0a56e7d40
Marcos Gabriel Lopes <marcos.lopes@cooperflora.com.br>
2023-04-28 17:02:36 -0300

    Services

---
+++ src/main/java/br/com/jurisconexao/auth/services/NotificationService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.jurisconexao.auth.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import br.com.jurisconexao.auth.models.Evento;
+import br.com.jurisconexao.auth.models.Notification;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Marcos
+ */
+public class NotificationService {
+    
+    public static void addNotification(Notification notification,String token) throws IOException, InterruptedException{
+                
+         
+ 
+        }
+    
+    
+}
