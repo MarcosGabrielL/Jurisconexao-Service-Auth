471f4bd21cc44d02920385bc80a895111558364a
Marcos Gabriel Lopes <marcos.lopes@cooperflora.com.br>
2023-04-28 16:55:32 -0300

    Exception

--- src/main/java/br/com/jurisconexao/auth/exception/UserNotFoundException.java
+++ src/main/java/br/com/jurisconexao/auth/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.jurisconexao.auth.Auth.exception;
+package br.com.jurisconexao.auth.exception;
 
 /**
  *
