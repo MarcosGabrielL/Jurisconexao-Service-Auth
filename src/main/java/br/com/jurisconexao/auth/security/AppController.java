b91e0413b7e03e3ea1fdc844602cd6e158232233
Marcos Gabriel <52946597+MarcosGabrielL@users.noreply.github.com>
2023-05-22 17:07:27 -0300

    Update AppController.java

--- src/main/java/br/com/jurisconexao/auth/security/AppController.java
+++ src/main/java/br/com/jurisconexao/auth/security/AppController.java
@@ -47,7 +47,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@CrossOrigin(origins = "*", allowedMethods = {"GET", "POST", "PUT", "DELETE"})
 public class AppController {
     
     @Autowired
