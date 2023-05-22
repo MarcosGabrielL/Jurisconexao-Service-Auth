a552c5ea320bd48c521840ce210927a91679817f
Marcos Gabriel Lopes <marcos.lopes@cooperflora.com.br>
2023-05-22 16:03:20 -0300

    update Revendedro

--- src/main/java/br/com/jurisconexao/auth/models/Revendedor.java
+++ src/main/java/br/com/jurisconexao/auth/models/Revendedor.java
@@ -38,64 +38,6 @@ public class Revendedor {
     private String codigo;
 
    
-    public Revendedor(Long id, String email, String password, String firstName, String lastName, String codigo) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.codigo = codigo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-    
-     
     
     
 }
